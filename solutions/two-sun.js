77143931b3df4239112f1e6a3ddb7eb48ca6b95d
dognie <dognie7@gmail.com>
2017-08-09 18:54:58 +0800

    two-sun

---
+++ solutions/two-sun.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/two-sum/description/
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+  var a = [];
+  for (var i = 0, len = nums.length; i < len; i++) {
+    var tmp = target - nums[i];
+    if (a[tmp] !== undefined) return [a[tmp], i];
+    a[nums[i]] = i;
+  }
+};
